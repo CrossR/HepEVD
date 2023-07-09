e29bd5a133451e2d66cbf54ea21efda3f394ed49
CrossR <ryan.cross@warwick.ac.uk>
2023-07-09 15:11:49 +0100

    Add the ability to render different 3D-hits, based on their property.

--- web/hep_evd.js
+++ web/hep_evd.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { Lut } from 'three/addons/math/Lut.js'
+import { Lut } from "three/addons/math/Lut.js";
 
 import Stats from "three/addons/libs/stats.module.js";
 
@@ -8,10 +8,13 @@ import Stats from "three/addons/libs/stats.module.js";
 // Helper functions
 // ============================================================================
 
+// Convert a JS position object to threejs.
 function positionToVector(position) {
   return new THREE.Vector3(position.x, position.y, position.z);
 }
 
+// Given a geometry object, fit the camera to the center of it, such that the
+// whole object can be seen.
 function fitSceneInCamera(camera, controls, detectorGeometry) {
   const offset = 1.5; // Padding factor.
 
@@ -49,10 +52,41 @@ function fitSceneInCamera(camera, controls, detectorGeometry) {
   }
 }
 
+// Build up a <string, array[hits]> map, linking every available hit property.
+function getHitProperties(hits) {
+  const hitPropMap = new Map();
+
+  // Create a default entry for energy and time,
+  // since every hit should have this info.
+  hitPropMap.set("energy", hits);
+  hitPropMap.set("time", hits);
+
+  const setOrAdd = (hit, prop) => {
+    if (hitPropMap.has(prop)) {
+      hitPropMap.get(prop).push(hit);
+    } else {
+      hitPropMap.set(prop, [hit]);
+    }
+  };
+
+  hits.forEach((hit) => {
+    if (Object.hasOwn(hit, "label")) {
+      setOrAdd(hit, hit.label);
+    }
+
+    if (Object.hasOwn(hit, "properties")) {
+      hit.properties.forEach((prop) => setOrAdd(hit, prop));
+    }
+  });
+
+  return hitPropMap;
+}
+
 // ============================================================================
 // Rendering Functions
 // ============================================================================
 
+// Given a box based detector volume, draw its wireframe.
 function drawBoxVolume(group, material, box) {
   const boxGeometry = new THREE.BoxGeometry(box.xWidth, box.yWidth, box.zWidth);
   const boxEdges = new THREE.EdgesGeometry(boxGeometry);
@@ -64,21 +98,44 @@ function drawBoxVolume(group, material, box) {
   group.add(boxLines);
 }
 
-function drawThreeDHits(group, material, hits, hit_size = 5) {
-  const hitGeometry = new THREE.BoxGeometry(hit_size, hit_size, hit_size);
+// Draw an array of 3D hits to the screen, utilising an InstancedMesh for
+// performance.
+//
+// Optionally, colour the hits based on some property value.
+function drawThreeDHits(
+  group,
+  material,
+  hits,
+  useColour = false,
+  colourProp = "",
+  hitSize = 3
+) {
+  const hitGeometry = new THREE.BoxGeometry(hitSize, hitSize, hitSize);
   const hitMesh = new THREE.InstancedMesh(hitGeometry, material, hits.length);
 
   const dummyObject = new THREE.Object3D();
+  const energyLut = new Lut("rainbow", 512);
 
-  const energyLut = new Lut('rainbow', 512);
-  energyLut.setMax(hits.reduce((max, hit) => hit.e > max ? hit.e : max, 0));
+  if (useColour) {
+    energyLut.setMax(
+      hits.reduce(
+        (max, hit) => (hit[colourProp] > max ? hit[colourProp] : max),
+        0
+      )
+    );
+  }
 
   hits.forEach(function (hit, index) {
     dummyObject.position.set(hit.x, hit.y, hit.z);
     dummyObject.updateMatrix();
 
     hitMesh.setMatrixAt(index, dummyObject.matrix);
-    hitMesh.setColorAt(index, energyLut.getColor(hit.e));
+    if (useColour) {
+      console.log(hit[colourProp])
+      hitMesh.setColorAt(index, energyLut.getColor(hit[colourProp]));
+    } else {
+      hitMesh.setColorAt(index, new THREE.Color('gray'));
+    }
   });
 
   hitMesh.instanceMatrix.needsUpdate = true;
@@ -91,6 +148,47 @@ function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
   stats.update();
+}
+
+// ============================================================================
+// GUI Functions
+// ============================================================================
+
+function threeDHitsToggle(
+  threeDHitGroupMap,
+  hitPropertiesMap,
+  toggleTarget
+) {
+
+  if (toggleTarget === "none") {
+    threeDHitGroupMap.forEach((group) => (group.visible = false));
+    return;
+  }
+
+  const hasGroup = threeDHitGroupMap.has(toggleTarget) === true;
+  const targetExists = hitPropertiesMap.has(toggleTarget) === true;
+
+  // If the property doesn't exist, we can't do anything.
+  if (! hasGroup && ! targetExists) {
+    return;
+  }
+
+  // If it does exist, then just toggle its visibility.
+  if (hasGroup) {
+    const threeDHitGroup = threeDHitGroupMap.get(toggleTarget);
+    threeDHitGroup.visible = !threeDHitGroup.visible;
+
+    return;
+  }
+
+  // If we are here, the property exists, but we haven't actually rendered it
+  // yet.  So make a new group, populate it, store it for later.
+  const newGroup = new THREE.Group();
+  scene.add(newGroup);
+  drawThreeDHits(newGroup, materialHit, hitPropertiesMap.get(toggleTarget), true, toggleTarget);
+  threeDHitGroupMap.set(toggleTarget, newGroup);
+
+  return;
 }
 
 // ============================================================================
@@ -124,21 +222,24 @@ document.body.appendChild(stats.dom);
 const materialLine = new THREE.LineBasicMaterial({ color: "gray" });
 const materialGeometry = new THREE.LineBasicMaterial({ color: "darkred" });
 const materialHit = new THREE.MeshBasicMaterial({
-  color: "gray",
   side: THREE.DoubleSide,
-  flatShared: true
 });
 
 const detectorGeometryGroup = new THREE.Group();
 scene.add(detectorGeometryGroup);
+
+// 3D hits are stored in multiple groups, such that they can be toggled independantly.
+const threeDHitGroupMap = new Map();
 const threeDHitGroup = new THREE.Group();
 scene.add(threeDHitGroup);
+threeDHitGroupMap.set("default", threeDHitGroup);
 
 // Finally, start pulling in data about the event.
 const detectorGeometry = await fetch("geometry").then((response) =>
   response.json()
 );
 const hits = await fetch("hits").then((response) => response.json());
+const hitPropertiesMap = getHitProperties(hits);
 
 // Time to start the actual rendering.
 detectorGeometry
@@ -151,6 +252,11 @@ drawThreeDHits(
   materialHit,
   hits.filter((hit) => hit.type === "3D")
 );
+
+// Populate the UI properly.
+// This includes functions that the GUI uses, and filling in the various dropdowns.
+document.threeDHitsToggle = (toggleTarget) =>
+  threeDHitsToggle(threeDHitGroupMap, hitPropertiesMap, toggleTarget);
 
 // Start the final rendering of the event.
 
