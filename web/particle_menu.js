61b5ef5a63fb0625f66dfbe73f1a6e2ec2f2792d
CrossR <ryan.cross@warwick.ac.uk>
2023-08-30 19:08:15 +0100

    Tidy up particle menu.
    Removed toggling of the vertex and hits independently.
    It's a pain, but I may come back to it if/when I refactor the
    filtering code to be a bit more modular.

--- web/particle_menu.js
+++ web/particle_menu.js
@@ -6,6 +6,16 @@
 
 import { INTERACTION_TYPE_SCORE } from "./constants.js";
 
+/**
+ * Creates a menu item for a particle with the given hit dimension, particle object, and onClick function.
+ * 
+ * @param {number} hitDim - The hit dimension of the particle.
+ * @param {Object} particle - The particle object.
+ * @param {Function} onClick - The function to be called when the menu item is clicked.
+ * @param {Map} particlesMap - The map of particles.
+ * @param {HTMLElement} parentElement - The parent element to which the menu item will be appended.
+ * @returns {void}
+ */
 function createMenuItem(
   hitDim,
   particle,
@@ -13,87 +23,76 @@ function createMenuItem(
   particlesMap,
   parentElement
 ) {
+  // Make the top level menu item for the particle.
   const menuItem = document.createElement("li");
   menuItem.id = `particle_${particle.id}_${hitDim}`;
   menuItem.classList.add("block");
 
+  // Make an optional details element for the particle, which will
+  // only be used if the particle has child particles.
   const details = document.createElement("details");
+  const summary = document.createElement("summary");
   details.open = false;
 
-  const summary = document.createElement("summary");
+  // This is the label, which tells the user the particle type and
+  // number of hits.
   const label = document.createElement("span");
   label.classList.add("label-text", "pr-4");
   label.addEventListener("click", () => {
-    onClick(particle, particlesMap, "All");
+    onClick(particle, particlesMap);
   });
 
-  // Now, add the child elements for the particle
-  // Add a new label for each of the child elements.
-  // In the case of the "Child PFOs" element, we need to
-  // recursively call this function to add the child particles
-  // to the menu.
+  // If there are any child particles, we need to create a sub-menu
+  // for them. To do that, we do a recursive call to this function,
+  // but with a new parent element, to nest them under the current
+  // particle.
   let totalNumHits = particle.hits.length;
   const elementList = document.createElement("ul");
-  ["Vertices", "Clusters", "Child PFOs"].forEach((childType) => {
-    if (childType === "Child PFOs" && particle.childIDs.length > 0) {
-      const details = document.createElement("details");
-      details.open = false;
 
-      const summary = document.createElement("summary");
-      const childLabel = document.createElement("span");
-      childLabel.innerHTML = childType;
-      childLabel.classList.add("label-text");
-      childLabel.addEventListener("click", () => {
-        onClick(particle, particlesMap, childType);
-      });
-      summary.appendChild(childLabel);
-      details.appendChild(summary);
+  particle.childIDs.map((childID) => {
+    const childParticle = particlesMap.get(childID);
 
-      particle.childIDs.map((childID) => {
-        const childParticle = particlesMap.get(childID);
-
-        // INFO: Likely a particle with no valid hits for this dimension.
-        if (childParticle === undefined) {
-          return;
-        }
-
-        createMenuItem(hitDim, childParticle, onClick, particlesMap, details);
-
-        // Lets also update the parent label to include the number of child
-        // particle hits.
-        totalNumHits += childParticle.hits.length;
-      });
-
-      elementList.appendChild(details);
-      return;
-    } else if (childType === "Child PFOs" && particle.childIDs.length === 0) {
+    // INFO: Likely a particle with no valid hits for this dimension.
+    if (childParticle === undefined) {
       return;
     }
 
-    const childItem = document.createElement("li");
-    const childLabel = document.createElement("span");
-    childLabel.innerHTML = childType;
-    childLabel.classList.add("label-text");
-    childLabel.addEventListener("click", () => {
-      onClick(particle, particlesMap, childType);
-    });
-    childItem.appendChild(childLabel);
-    elementList.appendChild(childItem);
+    // Recursively create the menu item for the child particle.
+    // This can be called multiple times, if the child particle
+    // has child particles of its own.
+    createMenuItem(hitDim, childParticle, onClick, particlesMap, elementList);
+    totalNumHits += childParticle.hits.length;
   });
 
-  // Finally set the label to include the total number of hits.
+  // Set the label text to include the number of hits, including the
+  // child particles hits.
   label.innerHTML = `${particle.interactionType} (${totalNumHits})`;
-  summary.appendChild(label);
 
-  // Then add the summary + dropdown bits to the new Particle...
-  details.appendChild(summary);
-  details.appendChild(elementList);
+  // If there are any child particles, we should include
+  // the details and summary elements in the menu item.
+  // This will give us the dropdown arrow to expand the
+  // sub-menu.
+  if (elementList.childElementCount > 0) {
+    summary.appendChild(label);
+    details.appendChild(summary);
+    details.appendChild(elementList);
+    menuItem.appendChild(details);
+  } else {
+    menuItem.appendChild(label);
+  }
 
-  // And finally add the new Particle to the menu.
-  menuItem.appendChild(details);
+  // Finally, append the menu item to the parent element.
   parentElement.appendChild(menuItem);
 }
 
+/**
+ * Creates a particle menu with the given hit dimension, particles map, and onClick function.
+ * 
+ * @param {string} hitDim - The hit dimension.
+ * @param {Map} particlesMap - The particles map.
+ * @param {function} onClick - The onClick function.
+ * @returns {void}
+ */
 export function createParticleMenu(hitDim, particlesMap, onClick) {
   const menu = document.getElementById(`particle_menu_${hitDim}`);
 
@@ -123,6 +122,11 @@ export function createParticleMenu(hitDim, particlesMap, onClick) {
 
       return aNumHits < bNumHits;
     });
+
+  if (particles.length === 0) {
+    menu.hidden = true;
+    return;
+  }
 
   particles.forEach((particle, _) => {
     createMenuItem(hitDim, particle, onClick, particlesMap, menu);
