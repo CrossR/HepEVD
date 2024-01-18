135280a24bab813522e871344481f2b392fc5418
CrossR <ryan.cross@warwick.ac.uk>
2024-01-18 15:46:17 +0000

    Update python test to use 2D hits.

--- example/test_python_bindings.py
+++ example/test_python_bindings.py
@@ -20,7 +20,7 @@ hep_evd.set_geo(detector_geometry)
 print(f"Checking if initialised: {hep_evd.is_init()}")
 
 print("Add some hits...")
-hits = [
+threeD_hits = [
     [
         [
             random.uniform(-350, 350),
@@ -31,19 +31,34 @@ hits = [
     ]
     for _ in range(25000)
 ]
-hep_evd.add_hits(hits)
+views = [hep_evd.HIT_TYPE.TWO_D_U, hep_evd.HIT_TYPE.TWO_D_V, hep_evd.HIT_TYPE.TWO_D_W]
+twoD_hits = [
+    [
+        [
+            random.uniform(-350, 350),
+            0.0,
+            random.uniform(0, 1300),
+        ],
+        random.uniform(0, 25),
+        hep_evd.HIT_DIM.TWO_D,
+        view,
+    ]
+    for _ in range(5000) for view in views
+]
+hep_evd.add_hits(threeD_hits)
+hep_evd.add_hits(twoD_hits)
 
 print("Save the current state...")
 hep_evd.set_verbose(True)
 hep_evd.save_state("First")
 
 print("Add some more hits...")
-left_hits = [hit for hit in hits if hit[0][0] < 0]
+left_hits = [hit for hit in threeD_hits if hit[0][0] < 0]
 hep_evd.add_hits(left_hits)
 hep_evd.save_state("Second")
 
 print("Adding even more hits...")
-right_hits = [hit for hit in hits if hit[0][0] > 0]
+right_hits = [hit for hit in threeD_hits if hit[0][0] > 0]
 hep_evd.add_hits(right_hits)
 hep_evd.save_state("Third")
 
