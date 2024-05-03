081fa950bfc9015b5e91bc05db668f03199b14bc
CrossR <ryan.cross@warwick.ac.uk>
2024-05-03 17:48:06 +0100

    Drop unused properties from JSON.

--- include/state.h
+++ include/state.h
@@ -99,8 +99,7 @@ class GUIConfig {
 
     // Only need a to JSON method, as we don't need to read in the state.
     // We also only want to pass the metadata, not the actual data.
-    NLOHMANN_DEFINE_TYPE_INTRUSIVE(GUIConfig, showParticles, showHits, showMCHits, showMarkers, showMCParticles, show2D,
-                                   show3D, renderGeometry, detector, hits);
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(GUIConfig, show2D, show3D, hits);
 };
 
 } // namespace HepEVD
