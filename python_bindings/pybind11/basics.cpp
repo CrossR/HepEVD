327674c6c03ef38d8f205819915735380683dbae
CrossR <ryan.cross@warwick.ac.uk>
2024-07-23 11:21:15 +0100

    Start to swap to nanobind.

--- python_bindings/pybind11/basics.cpp
+++ python_bindings/pybind11/basics.cpp
@@ -1,5 +1,4 @@
-#include <pybind11/numpy.h>
-#include <pybind11/pybind11.h>
+#include <nanobind/nanobind.h>
 
 #include <cstdlib>
 #include <map>
@@ -12,18 +11,58 @@
 // And any local includes...
 #include "../include/detectors.hpp"
 
-namespace py = pybind11;
+namespace nb = nanobind;
 
 // Map from Python types to HepEVD types.
 using RawHit = std::tuple<double, double, double, double>;
 using PythonHitMap = std::map<RawHit, HepEVD::Hit *>;
 inline PythonHitMap pythonHitMap;
 
-bool isArrayOrList(py::handle obj) { return py::isinstance<py::array>(obj) || py::isinstance<py::list>(obj); }
+bool isArrayOrList(nb::handle obj) { return nb::isinstance<nb::array>(obj) || nb::isinstance<nb::list>(obj); }
+
+std::vector<ssize_t> getShapeSize(nb::handle obj) {
+    if (nb::isinstance<nb::array>(obj)) {
+        auto info = obj.cast<nb::array>().request();
+        return info.shape;
+    } else if (nb::isinstance<nb::list>(obj)) {
+        auto list = obj.cast<nb::list>();
+
+        std::vector<ssize_t> listShape({static_cast<ssize_t>(list.size())});
+
+        try {
+            nb::handle firstListObject(list[0]);
+            std::cout << firstListObject << std::endl;
+            std::vector<ssize_t> firstShape = getShapeSize(firstListObject);
+
+        } catch (...) {
+        }
+
+        return listShape;
+    }
+
+    throw std::runtime_error("Not an array or list");
+}
+
+template <typename T>
+T getValueFromListOrArray(nb::handle obj, int index) {
+    
+    if (nb::isinstance<nb::array>(obj)) {
+        auto info = obj.cast<nb::array>().request();
+        return info.ptr[index];
+    } else if (nb::isinstance<nb::list>(obj)) {
+        auto info = getShapeSize(obj);
+
+        // We need to flatten the list, so that we can access the correct index.
+        std::vector<T> data;
+
+        for (int i = 0; i < info[0]; i++) {
+
+    }
+}
 
 // Set the current HepEVD geometry.
 // Input will either be a string or a list/array of numbers.
-void set_geometry(py::object geometry) {
+void set_geometry(nb::object geometry) {
 
     if (HepEVD::isServerInitialised())
         return;
@@ -32,29 +71,22 @@ void set_geometry(py::object geometry) {
 
     // If the input is a string, check if it is a detector.
     // Otherwise if an array or list, assume it is a list of volumes.
-    if (py::isinstance<py::str>(geometry)) {
+    if (nb::isinstance<nb::str>(geometry)) {
         if (detectors.find(geometry.cast<std::string>()) == detectors.end())
             throw std::runtime_error("Unknown detector: " + geometry.cast<std::string>());
 
         volumes = detectors[geometry.cast<std::string>()];
     } else if (isArrayOrList(geometry)) {
 
-        py::buffer_info info;
-
-        if (py::isinstance<py::array>(geometry)) {
-            info = geometry.cast<py::array>().request();
-        } else {
-            py::list list = geometry.cast<py::list>();
-            info = list[0].cast<py::array>().request();
-        }
+        auto info = getShapeSize(geometry);
 
         // TODO: Extend this to other geometry types.
-        if (info.shape[1] != 6)
+        if (info[1] != 6)
             throw std::runtime_error("Geometry array must have 6 columns");
 
         double *data = static_cast<double *>(info.ptr);
 
-        for (int i = 0; i < info.shape[0]; i++) {
+        for (int i = 0; i < info[0]; i++) {
             volumes.push_back(BoxVolume(Position({data[i * 6 + 0], data[i * 6 + 1], data[i * 6 + 2]}), data[i * 6 + 3],
                                         data[i * 6 + 4], data[i * 6 + 5]));
         }
@@ -71,12 +103,12 @@ void set_geometry(py::object geometry) {
 
 // Add the given list/array of hits to the server.
 // Works for either HepEVD::Hit or HepEVD::MCHit.
-template <typename T> void add_hits(py::buffer hits, std::string label = "") {
+template <typename T> void add_hits(nb::buffer hits, std::string label = "") {
 
     if (!HepEVD::isServerInitialised())
         return;
 
-    py::buffer_info hits_info = hits.request();
+    nb::buffer_info hits_info = hits.request();
 
     if (hits_info.ndim != 2)
         throw std::runtime_error("Hits array must be 2D");
@@ -122,12 +154,12 @@ template <typename T> void add_hits(py::buffer hits, std::string label = "") {
 }
 
 // Apply properties to the given hit or hits.
-void set_hit_properties(py::buffer hit, py::dict properties) {
+void set_hit_properties(nb::buffer hit, nb::dict properties) {
 
     if (!HepEVD::isServerInitialised())
         return;
 
-    py::buffer_info hit_info = hit.request();
+    nb::buffer_info hit_info = hit.request();
 
     if (hit_info.ndim != 1)
         throw std::runtime_error("Hit array must be 1D");
@@ -150,38 +182,38 @@ void set_hit_properties(py::buffer hit, py::dict properties) {
     }
 }
 
-PYBIND11_MODULE(hep_evd, m) {
+NB_MODULE(hep_evd, m) {
 
     m.doc() = "HepEVD - High Energy Physics Event Display";
 
-    m.def("start_server", &HepEVD::startServer, "Starts the HepEVD server", py::arg("start_state") = -1,
-          py::arg("clear_on_show") = true);
+    m.def("start_server", &HepEVD::startServer, "Starts the HepEVD server", nb::arg("start_state") = -1,
+          nb::arg("clear_on_show") = true);
     m.def("is_initialised", &HepEVD::isServerInitialised,
           "Checks if the server is initialised - i.e. does a server exists, with the geometry set?");
-    m.def("set_verbose", &HepEVD::setVerboseLogging, "Sets the verbosity of the HepEVD server", py::arg("verbose"));
+    m.def("set_verbose", &HepEVD::setVerboseLogging, "Sets the verbosity of the HepEVD server", nb::arg("verbose"));
 
-    m.def("save_state", &HepEVD::saveState, "Saves the current state", py::arg("state_name"), py::arg("min_size") = -1,
-          py::arg("clear_on_show") = true);
-    m.def("reset_server", &HepEVD::resetServer, "Resets the server", py::arg("reset_geo") = false);
+    m.def("save_state", &HepEVD::saveState, "Saves the current state", nb::arg("state_name"), nb::arg("min_size") = -1,
+          nb::arg("clear_on_show") = true);
+    m.def("reset_server", &HepEVD::resetServer, "Resets the server", nb::arg("reset_geo") = false);
 
-    m.def("set_geometry", &set_geometry, "Sets the geometry of the server", py::arg("geometry"));
+    m.def("set_geometry", &set_geometry, "Sets the geometry of the server", nb::arg("geometry"));
 
     m.def("add_hits", &add_hits<HepEVD::Hit>,
           "Adds hits to the current event state. Hits must be passed an (N, 4) list or array, with the 4 columns being "
           "(x, y, z, energy)",
-          py::arg("hits"), py::arg("label") = "");
+          nb::arg("hits"), nb::arg("label") = "");
     m.def("add_mc", &add_hits<HepEVD::MCHit>,
           "Adds hits to the current event state. Hits must be passed an (N, 5) list or array, with the 5 columns being "
           "(x, y, z, energy, PDG)",
-          py::arg("mcHits"), py::arg("label") = "");
+          nb::arg("mcHits"), nb::arg("label") = "");
     m.def("add_hit_properties", &set_hit_properties, "Add custom properties to a hit, via a string / double dictionary",
-          py::arg("hit"), py::arg("properties"));
+          nb::arg("hit"), nb::arg("properties"));
 
-    py::enum_<HepEVD::HitType>(m, "HitType")
+    nb::enum_<HepEVD::HitType>(m, "HitType")
         .value("GENERAL", HepEVD::GENERAL)
         .value("TWO_D_U", HepEVD::TWO_D_U)
         .value("TWO_D_V", HepEVD::TWO_D_V)
         .value("TWO_D_W", HepEVD::TWO_D_W);
 
-    py::enum_<HepEVD::HitDimension>(m, "HitDimension").value("TWO_D", HepEVD::TWO_D).value("THREE_D", HepEVD::THREE_D);
+    nb::enum_<HepEVD::HitDimension>(m, "HitDimension").value("TWO_D", HepEVD::TWO_D).value("THREE_D", HepEVD::THREE_D);
 }
--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -3,7 +3,6 @@
 # Prior to Python 3.12 distutils was the standard way to build python extensions
 # distutils is now deprecated in favor of setuptools
 
-from glob import glob
 import os
 
 try:
@@ -13,22 +12,6 @@ except ImportError:
 
 include_dirs = ["../"]
 define_macros = []
-
-use_pybind11 = False
-
-try:
-    import pybind11
-    from pybind11.setup_helpers import Pybind11Extension
-
-    print("pybind11 is installed, bindings will be built with pybind11 support.")
-    include_dirs.append(pybind11.get_include())
-    define_macros.append(("USE_PYBIND11", 1))
-    use_pybind11 = True
-except ImportError:
-    print("pybind11 is missing, bindings will be built without pybind11 support.")
-    print(
-        "The bindings without pybind11 support are much more basic, but should still work."
-    )
 
 try:
     import numpy
@@ -42,12 +25,7 @@ try:
     # Numpy 1.7 deprecated the NPY_NO_DEPRECATED_API macro
     define_macros.append(("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"))
 except ImportError:
-    if not use_pybind11:
-        print("Numpy is missing, bindings will be built without numpy support.")
-    else:
-        print("Numpy is missing, but pybind11 is installed, please install numpy.")
-        print("Bindings for PyBind11 require Numpy.")
-        exit(1)
+    print("Numpy is missing, bindings will be built without numpy support.")
 
 # Get the absolute path to the ../web directory,
 # so it can be used regardless of where HepEVD is installed.
@@ -68,14 +46,9 @@ extension_kwargs = {
     "define_macros": define_macros,
 }
 
-if not use_pybind11:
-    hep_evd = Extension(
-        "hep_evd", sources=["basic_hep_evd_bindings.cpp"], **extension_kwargs
-    )
-else:
-    hep_evd = Pybind11Extension(
-        "hep_evd", sorted(glob("pybind11/*.cpp")), **extension_kwargs
-    )
+hep_evd = Extension(
+    "hep_evd", sources=["basic_hep_evd_bindings.cpp"], **extension_kwargs
+)
 
 setup(
     name="hep_evd",
