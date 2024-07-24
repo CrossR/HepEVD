775e8fd9366acaab62dcf27bb601ad2be1abd82c
CrossR <ryan.cross@warwick.ac.uk>
2024-07-24 12:20:41 +0100

    Restructure tests to match C++.

--- example/test_python_bindings.py
+++ example/test_python_bindings.py
@@ -3,97 +3,98 @@ import numpy as np
 
 import HepEVD
 
-# Test Python Bindings
-print("Testing Python Bindings")
 
-# Check if initialised
-print(f"Checking if initialised: {HepEVD.is_initialised()}")
-
-# Initialise
-print("Initialising...")
-detector_geometry = [
-    [-182.954544067, 0, 696.293762207, 359.415008545, 1207.84753418, 1394.33996582],
-    [182.954544067, 0, 696.293762207, 359.415008545, 1207.84753418, 1394.33996582],
-]
-
-print("Set geometry with objects...")
-HepEVD.set_geometry(detector_geometry)
-
-# Should now be initialised, then reset and set geometry with name
-print(f"Checking if initialised: {HepEVD.is_initialised()}")
-HepEVD.reset_server(True)
-print(f"Checking if initialised: {HepEVD.is_initialised()}")
-
-print("Set geo with name...")
-HepEVD.set_geometry("dunefd_1x2x6")
-
-# Should now be initialised
-print(f"Checking if initialised: {HepEVD.is_initialised()}")
-
-print("Add some hits...")
-threeD_hits = [
-    [random.uniform(-350, 350), random.uniform(-600, 600), random.uniform(0, 1300),]
-    for _ in range(25000)
-]
-
-# Update the hits to include an energy, which is just the sum of the coordinates
-threeD_hits = np.array([[*hit, sum(hit)] for hit in threeD_hits])
-
-views = [HepEVD.HitType.TWO_D_U, HepEVD.HitType.TWO_D_V, HepEVD.HitType.TWO_D_W]
-twoD_hits = [
-    [
-        random.uniform(-350, 350),
-        0.0,
-        random.uniform(0, 1300),
-        random.uniform(0, 25),
-        HepEVD.HitDimension.TWO_D,
-        view,
+def main() -> None:
+    # DUNE FD 1x2x6-like geometry
+    detector_geometry = [
+        [-182.954544067, 0, 696.293762207, 359.415008545, 1207.84753418, 1394.33996582],
+        [182.954544067, 0, 696.293762207, 359.415008545, 1207.84753418, 1394.33996582],
     ]
-    for _ in range(5000)
-    for view in views
-]
+    assert not HepEVD.is_initialised()
+    HepEVD.set_geometry(detector_geometry)
 
-print("Adding 3D hits from numpy array...")
-HepEVD.add_hits(threeD_hits)
+    # Should now be initialised, then reset and set geometry with name
+    assert HepEVD.is_initialised()
+    HepEVD.reset_server(True)
+    assert not HepEVD.is_initialised()
 
-print("Adding 2D hits from python list...")
-HepEVD.add_hits(twoD_hits)
+    # Also test set_geometry with string, loading the predefined geometry.
+    HepEVD.set_geometry("dunefd_1x2x6")
+    assert HepEVD.is_initialised()
 
-print("Add propreties to all hits...")
-for hit in threeD_hits:
-    properties = {}
+    # Generate some 3D hits.
+    # Stored as a numpy array of numpy arrays.
+    num_hits_3D = 25000
+    threeD_hits = np.stack(
+        (
+            np.random.uniform(-350, 350, num_hits_3D),
+            np.random.uniform(-600, 600, num_hits_3D),
+            np.random.uniform(0, 1300, num_hits_3D),
+        ),
+        axis=1,
+    )
 
-    if hit[0] < -250:
-        properties["Left"] = 1.0
-    if hit[0] > 250:
-        properties["Right"] = 1.0
+    # Define the energy of the hits as the sum of the coordinates
+    hit_sum = np.sum(threeD_hits, axis=1)
+    threeD_hits = np.concatenate((threeD_hits, hit_sum[:, np.newaxis]), axis=1)
 
-    if hit[1] < -500:
-        properties["Bottom"] = 1.0
-    if hit[1] > 500:
-        properties["Top"] = 1.0
+    # Now add some 2D hits
+    # Stored as a list of lists.
+    views = [HepEVD.HitType.TWO_D_U, HepEVD.HitType.TWO_D_V, HepEVD.HitType.TWO_D_W]
+    twoD_hits = [
+        [
+            random.uniform(-350, 350),
+            0.0,
+            random.uniform(0, 1300),
+            random.uniform(0, 25),
+            HepEVD.HitDimension.TWO_D,
+            view,
+        ]
+        for _ in range(5000)
+        for view in views
+    ]
 
-    if hit[2] < 100:
-        properties["Front"] = 1.0
-    if hit[2] > 1200:
-        properties["Back"] = 1.0
+    # Add the both sets of hits
+    HepEVD.add_hits(threeD_hits)
+    HepEVD.add_hits(twoD_hits)
 
-    HepEVD.add_hit_properties(hit, properties)
+    # Add some properties to the 3D hits
+    for hit in threeD_hits:
+        properties = {}
 
-print("Save the current state...")
-HepEVD.save_state("First")
+        if hit[0] < -250:
+            properties["Left"] = 1.0
+        if hit[0] > 250:
+            properties["Right"] = 1.0
 
-print("Add some more hits...")
-left_hits = [hit for hit in threeD_hits if hit[0] < 0]
-HepEVD.add_hits(left_hits)
-HepEVD.save_state("Second")
+        if hit[1] < -500:
+            properties["Bottom"] = 1.0
+        if hit[1] > 500:
+            properties["Top"] = 1.0
 
-print("Adding even more hits...")
-right_hits = np.array([[*hit] for hit in threeD_hits if hit[0] > 0])
-HepEVD.add_hits(right_hits)
-HepEVD.save_state("Third")
+        if hit[2] < 100:
+            properties["Front"] = 1.0
+        if hit[2] > 1200:
+            properties["Back"] = 1.0
 
-print("Testing event display")
-HepEVD.start_server()
+        HepEVD.add_hit_properties(hit, properties)
 
-print("Finished testing Python Bindings!")
+    # Test the state functionality
+    HepEVD.save_state("First")
+
+    # New state with only hits on the left of the detector
+    left_hits = [hit for hit in threeD_hits if hit[0] < 0]
+    HepEVD.add_hits(left_hits)
+    HepEVD.save_state("Second")
+
+    # And only hits on the right of the detector
+    right_hits = np.array([[*hit] for hit in threeD_hits if hit[0] > 0])
+    HepEVD.add_hits(right_hits)
+    HepEVD.save_state("Third")
+
+    HepEVD.start_server()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
